5dad8bb87eaeaf2c59ab546efaacd430f799ea08
yura95st <yura95st@gmail.com>
2017-03-14 01:10:00 +0200

    Implement apriori algorithm.

---
+++ apriori.py
@@ -0,0 +1,97 @@
+from collections import defaultdict
+
+
+class Apriori:
+
+    def __init__(self, transactions, min_support, min_confidence):
+        self._transactions = transactions
+        self._min_support = min_support
+        self._min_confidence = min_confidence
+
+        self._support = defaultdict(int)
+
+    def perform(self):
+        elements = self._get_elements()
+
+        rules = self._get_rules(elements[-1])
+
+        return (elements, rules)
+
+    def _get_elements(self):
+        result = []
+
+        for transaction in self._transactions:
+            for item in transaction:
+                self._support[frozenset(item)] += 1
+
+        candidates = self._support.keys()
+
+        while candidates:
+            frequent_candidates = self._get_frequent_candidates(candidates)
+            if not frequent_candidates:
+                break
+
+            result.append(frequent_candidates)
+
+            nonfrequent_candidates = candidates - frequent_candidates
+
+            candidates = self._generate_candidates(frequent_candidates)
+            candidates = self._prune(candidates, nonfrequent_candidates)
+
+            self._calculate_support(candidates)
+
+        return result
+
+    def _get_rules(self, candidates):
+        rules = {}
+
+        for cand in candidates:
+            all_subsets = self._get_all_subsets(cand)
+
+            for head in [frozenset(item) for item in all_subsets]:
+                if not head or head == cand:
+                    continue
+
+                confidence = self._support[cand] / self._support[head]
+
+                if confidence < self._min_confidence:
+                    continue
+
+                tail = cand - head
+
+                rules[(head, tail)] = confidence
+
+        max_confidence = max(rules.values())
+
+        return [rule for rule in rules if rules[rule] == max_confidence]
+
+    def _get_frequent_candidates(self, candidates):
+        return {c for c in candidates if self._support[c] >= self._min_support}
+
+    def _generate_candidates(self, frequent_candidates):
+        frequent_candidates = sorted([sorted(item)
+                                      for item in frequent_candidates])
+
+        for i, item1 in enumerate(frequent_candidates):
+            for j in range(i + 1, len(frequent_candidates)):
+                item2 = frequent_candidates[j]
+
+                if item1[:-1] == item2[:-1]:
+                    yield frozenset(item1 + item2[-1:])
+
+    def _prune(self, candidates, nonfrequent_candidates):
+        return {cand for cand in candidates for nf_cand in nonfrequent_candidates if not nf_cand.issubset(cand)}
+
+    def _calculate_support(self, candidates):
+        for cand in candidates:
+            for transaction in self._transactions:
+                if cand.issubset(transaction):
+                    self._support[cand] += 1
+
+    def _get_all_subsets(self, my_set):
+        result = [[]]
+
+        for x in my_set:
+            result = result + [y + [x] for y in result]
+
+        return result
--- main.py
+++ main.py
@@ -1,5 +1,39 @@
-def main():
-    pass
+import csv
+import operator
+from apriori import Apriori
+
+
+def get_transactions_from_file(filename):
+    result = []
+
+    with open(filename, 'r') as csvfile:
+        lines = csv.reader([line for line in csvfile if line.strip()])
+
+        for line in lines:
+            result.append({item for item in line})
+
+    return result
+
+
+def main(filename='data/data.txt', min_support=4, min_confidence=0):
+    transactions = get_transactions_from_file(filename)
+
+    apriori = Apriori(transactions, min_support, min_confidence)
+
+    elements, rules = apriori.perform()
+
+    print('Elements:')
+    for elem in sorted(elements):
+        for item in sorted([sorted(item) for item in elem]):
+            print(item)
+        print()
+
+    print('Rules:')
+    rules = sorted(rules, key=lambda x: list(x[0]))
+    rules = sorted(rules, key=lambda x: len(x[0]))
+
+    for head, tail in rules:
+        print('{} -> {}'.format(sorted(head), sorted(tail)))
 
 
 if __name__ == '__main__':
